9aaa92f4af3c0128d333a5a2cab6fe32eb046b86
Federico <federico.caiello@gmail.com>
2020-01-20 19:43:33 +0100

    creazione dropdown

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(
+  function () {
+    $('.parent-dropdown > a').mouseover(
+      function (){
+        $(this).next('.dropdown').toggleClass('active');
+
+        $('.parent-dropdown > a').click(
+          function (){
+            $(this).next('.dropdown').toggleClass('active');
+          }
+        );
+
+      }
+    );
+  }
+);
